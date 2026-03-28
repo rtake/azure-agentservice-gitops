c5fcaedb491706f3fa1db314f072ccb3d140bff0
rtake <rtake1110@gmail.com>
2026-03-28 10:34:44 +0900

    readonly API_VERSION

--- .github/scripts/deploy_agent/env.sh
+++ .github/scripts/deploy_agent/env.sh
@@ -4,7 +4,6 @@ readonly SUBSCRIPTION_ID="${AZURE_SUBSCRIPTION_ID:?AZURE_SUBSCRIPTION_ID is requ
 readonly AIFOUNDRY_ACCOUNT_NAME="${AIFOUNDRY_ACCOUNT_NAME:?AIFOUNDRY_ACCOUNT_NAME is required}"
 readonly RESOURCE_GROUP_NAME="${RESOURCE_GROUP_NAME:?RESOURCE_GROUP_NAME is required}"
 readonly PROJECT_NAME="${PROJECT_NAME:?PROJECT_NAME is required}"
-readonly API_VERSION="${API_VERSION:?API_VERSION is required}"
 readonly MODEL_CONFIG_FILE="${MODEL_CONFIG_FILE:-azure/infra/model-config.json}"
 
 foundry_base_url() {
@@ -21,12 +20,12 @@ arm_base_url() {
 
 application_url() {
   local app_name="$1"
-  printf '%s/applications/%s?api-version=%s' "$(arm_base_url)" "$app_name" "$API_VERSION"
+  printf '%s/applications/%s?api-version=%s' "$(arm_base_url)" "$app_name"
 }
 
 deployment_url() {
   local app_name="$1"
-  printf '%s/applications/%s/agentDeployments/%s?api-version=%s' "$(arm_base_url)" "$app_name" "$app_name" "$API_VERSION"
+  printf '%s/applications/%s/agentDeployments/%s?api-version=%s' "$(arm_base_url)" "$app_name" "$app_name"
 }
 
 fallback_deployment_id() {
